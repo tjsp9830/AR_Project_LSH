0a2366c3ed6b422f1457ec812925df7b749c2398
tjsp9830 <mcgr1998@gmail.com>
2024-10-11 02:36:52 +0900

    [Test] 페이스 트래킹 화장이미지 붙이기
    아직 버텍스와 이미지가 제대로 매핑되지 않은 상태입니다만,
    얼굴을 트래킹하여 이미지가 뜬다는 것을 확인했으므로 넘어갈 예정입니다.

---
+++ Assets/Scripts/LSH_StateChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LSH_StateChanger : MonoBehaviour
+{
+
+    [SerializeField] TextMeshPro CurState;
+
+
+
+
+}
